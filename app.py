cbe860f7331673d61acf5b33db0a01b0b53dce59
jick <jicortes1@miuandes.cl>
2021-11-06 23:51:35 -0300

    intial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, send_from_directory
+import flask_cors
+import gunicorn
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__, static_folder='client/build',static_url_path='')
+cors = CORS(app)
+
+
+@app.route('/api')
+@cross_origin()
+def Welcome():
+    return "Welcome to the API!!!"
+
+@app.route('/')
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
+
+
+if __name__ == '__main__':
+    app.run()
