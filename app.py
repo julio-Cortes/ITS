389de4a60f1e6d4513a1f7a232e52a27283dc4fd
jick <jicortes1@miuandes.cl>
2021-11-07 00:26:14 -0300

    feat(added return message)

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 
+import requests
 from flask import Flask, send_from_directory
 from flask import request
 import flask_cors
@@ -8,7 +9,7 @@ from flask_cors import CORS, cross_origin
 
 app = Flask(__name__, static_folder='client/build', static_url_path='')
 cors = CORS(app)
-
+BOT_URL = f'https://api.telegram.org/bot2110362248:AAFipt4bqziQLmmqmSdaCaw8e08L5UZYJCI/'
 
 @app.route('/api')
 @cross_origin()
@@ -24,7 +25,19 @@ def serve():
 @app.route("/api/webhook")
 @cross_origin()
 def get_webhook():
-    print(request.json)
+    data = request.json
+
+    print(data)  # Comment to hide what Telegram is sending you
+    chat_id = data['message']['chat']['id']
+    message = data['message']['text']
+
+    json_data = {
+        "chat_id": chat_id,
+        "text": message,
+    }
+
+    message_url = BOT_URL + 'sendMessage'
+    requests.post(message_url, json=json_data)
 
 
 if __name__ == '__main__':
